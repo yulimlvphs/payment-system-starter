0c2b1bec951364105da91b7302746513c94477b7
yulim <iojl1202@naver.com>
2026-08-09 14:44:28 +0900

    feat(product) : Product엔티티 및 Repository 구현

---
+++ src/main/java/com/sparta/paymentsystem/domain/product/entity/Product.java
@@ -0,0 +1,45 @@
+package com.sparta.paymentsystem.domain.product.entity;
+
+import com.sparta.paymentsystem.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    //4바이트 정수에서 음수를 허용하지 않아 값 범위가 0부터 4,294,967,295까지로 늘어나며, 음수가 의미 없는 수량·재고·ID 등에 주로 사용
+    @Column(nullable = false, columnDefinition = "int UNSIGNED")
+    private int price;
+
+    @Column(nullable = false, columnDefinition = "int UNSIGNED DEFAULT 0")
+    private int stock = 0;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public Product(String name, int price, int stock, String description) {
+        if (price < 0) {
+            throw new IllegalArgumentException("가격은 0 이상이어야 합니다");
+        }
+        if (stock < 0) {
+            throw new IllegalArgumentException("재고는 0 이상이어야 합니다");
+        }
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
+    }
+
+}
