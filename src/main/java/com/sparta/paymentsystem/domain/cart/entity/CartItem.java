4d97dfb47c6ba503b900b4aa101704c27c7f3590
yulim <iojl1202@naver.com>
2026-08-09 19:44:46 +0900

    feat(cart): CartItem 엔티티 및 Repository 구현

---
+++ src/main/java/com/sparta/paymentsystem/domain/cart/entity/CartItem.java
@@ -0,0 +1,65 @@
+package com.sparta.paymentsystem.domain.cart.entity;
+import com.sparta.paymentsystem.domain.member.entity.Member;
+import com.sparta.paymentsystem.domain.product.entity.Product;
+import com.sparta.paymentsystem.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"member_id", "product_id"})
+        //같은 회원이 같은 상품을 장바구니에 중복으로 담아도 (member_id, product_id) 조합당 행이 1개만 존재하도록 DB 레벨에서 중복을 원천 차단하기 위해서입니다.
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false, columnDefinition = "int UNSIGNED DEFAULT 1")
+    private int quantity;
+
+    public CartItem(Member member, Product product, int quantity) {
+        this.member = member;
+        this.product = product;
+        if (quantity < 1) {
+            throw new IllegalArgumentException("수량은 1 이상이어야 합니다.");
+        }
+        this.quantity = quantity;
+    }
+
+    public Long getMemberId() {
+        return member.getId();
+    }
+
+    public Long getProductId() {
+        return product.getId();
+    }
+
+    // quantity의 변경은 CartItem만 가능해야한다.
+    public void addQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("수량은 1 이상이어야 합니다.");
+        }
+        this.quantity += quantity;
+    }
+
+    public void changeQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("수량은 1 이상이어야 합니다.");
+        }
+        this.quantity = quantity;
+    }
+}
