2a6e9cc65b10b4a9bd25c35c0320174acbb8c02f
yulim <iojl1202@naver.com>
2026-08-11 15:42:23 +0900

    feat(payment): Payment 엔티티 및 PaymentRepository 구현

---
+++ src/main/java/com/sparta/paymentsystem/domain/payment/entity/Payment.java
@@ -0,0 +1,50 @@
+package com.sparta.paymentsystem.domain.payment.entity;
+
+import com.sparta.paymentsystem.domain.order.entity.Order;
+import com.sparta.paymentsystem.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment extends BaseTimeEntity { //돈이 어떻게 결제됐는가 ? 결제 수단, 결제 금액, 결제 상태
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
+    private Order order;
+
+    @Column(name = "portone_payment_id", nullable = false, unique = true, length = 200)
+    private String portonePaymentId;
+
+    @Column(nullable = false, columnDefinition = "int UNSIGNED")
+    private int amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private PaymentStatus status = PaymentStatus.IN_PROGRESS;
+
+    @Column(name = "paid_at")
+    private LocalDateTime paidAt;
+
+    public Payment(Order order, int amount) {
+        this.order = order;
+        this.amount = amount;
+        this.portonePaymentId = generatePortonePaymentId();
+    }
+
+    private static String generatePortonePaymentId() {
+        return "pay_" + UUID.randomUUID();
+    }
+
+}
