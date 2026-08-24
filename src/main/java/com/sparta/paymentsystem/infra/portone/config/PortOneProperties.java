35f8ba81537fda5c7aa84558b14971c883f3afe6
yulim <iojl1202@naver.com>
2026-08-24 20:05:49 +0900

    chore(portone): PortOneProperties 설정 바인딩 클래스 생성

---
+++ src/main/java/com/sparta/paymentsystem/infra/portone/config/PortOneProperties.java
@@ -0,0 +1,19 @@
+package com.sparta.paymentsystem.infra.portone.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+// application.yaml에 있는 portone: 이라고 되어있는 부분을 읽어서 가지고 옴.
+// 해당 값들을 가지고 와서 값들을 채우고, 객체로까지 만들어준다.
+@ConfigurationProperties(prefix = "portone")
+@Setter
+@Getter
+public class PortOneProperties {
+    private String baseUrl;
+    private String apiSecret;
+    private String storeId;
+    private String channelKey;
+}
