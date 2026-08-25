d3a83e4a9d734ffdbb728fd04fd4bb70e87046eb
yulim <iojl1202@naver.com>
2026-08-25 14:20:07 +0900

    한 번에 커밋

--- src/main/java/com/sparta/paymentsystem/infra/portone/controller/PortOneConfigController.java
+++ src/main/java/com/sparta/paymentsystem/infra/portone/controller/PortOneConfigController.java
@@ -14,6 +14,9 @@ public class PortOneConfigController {
 
     private final PortOneProperties portOneProperties;
 
+    // API는 결제를 수행하는 API가 아니라, 프론트엔드에 PortOne 결제창을 실행하는 데 필요한 설정값을 전달하는 API
+    // 이 요청의 목적 자체가 다음 두 값을 조회하는 것
+    // 왜 조회하냐? 결제 요청때 필요한 값들을 프론트에서 보이지 않게 변수로 처리하려고
     @GetMapping("/api/config/portone")
     public ResponseEntity<ApiResponse<PortOneConfigResponse>> getConfig() {
         return ResponseEntity.ok(ApiResponse.ok(new PortOneConfigResponse(
